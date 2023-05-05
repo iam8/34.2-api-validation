c487160d89c3b30cb63bc7ba0d6f796dab856ffd
alexm <alex-m@outlook.com>
2023-05-04 17:43:46 -0700

    Add my header; adjust order of some imports and statements; adjust indentation and export format

--- app.js
+++ app.js
@@ -1,34 +1,36 @@
+// Ioana A Mititean
+// Exercise 34.2 - Express Bookstore
+
 /** Express app for bookstore. */
 
 
 const express = require("express");
+const { ExpressError } = require("./expressError");
+
 const app = express();
 
 app.use(express.json());
 
-const ExpressError = require("./expressError")
+// Routes
 const bookRoutes = require("./routes/books");
-
 app.use("/books", bookRoutes);
 
+
 /** 404 handler */
-
 app.use(function (req, res, next) {
-  const err = new ExpressError("Not Found", 404);
-  return next(err);
+    const err = new ExpressError("Not Found", 404);
+    return next(err);
 });
 
 
-/** general error handler */
-
+/** General error handler */
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
+    res.status(err.status || 500);
 
-  return res.json({
-    error: err,
-    message: err.message
-  });
+    return res.json({
+        error: err,
+    });
 });
 
 
-module.exports = app;
+module.exports = { app };
