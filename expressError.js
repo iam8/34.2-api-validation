801bacd1c3135f74483f7d4ce9b412d6b7919980
alexm <alex-m@outlook.com>
2023-05-04 17:36:35 -0700

    Add header; adjust indentation and export format

--- expressError.js
+++ expressError.js
@@ -1,3 +1,7 @@
+// Ioana A Mititean
+// Exercise 34.2 - Express Bookstore
+
+
 /** ExpressError extends the normal JS error so we can easily
  *  add a status when we make an instance of it.
  *
@@ -5,13 +9,13 @@
  */
 
 class ExpressError extends Error {
-  constructor(message, status) {
-    super();
-    this.message = message;
-    this.status = status;
-    console.error(this.stack);
-  }
+    constructor(message, status) {
+        super();
+        this.message = message;
+        this.status = status;
+        console.error(this.stack);
+    }
 }
 
 
-module.exports = ExpressError;
+module.exports = { ExpressError };
