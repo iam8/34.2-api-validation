a37bcbfa5811c2fdffcdc9491b1bd7470215c1c3
alexm <alex-m@outlook.com>
2023-05-04 18:03:26 -0700

    Change id to isbn in GET /:id route (for consistency with other routes)

--- routes/books.js
+++ routes/books.js
@@ -20,10 +20,10 @@ router.get("/", async function (req, res, next) {
 });
 
 
-/** GET /[id]  => {book: book} */
-router.get("/:id", async function (req, res, next) {
+/** GET /[isbn]  => {book: book} */
+router.get("/:isbn", async function (req, res, next) {
     try {
-        const book = await Book.findOne(req.params.id);
+        const book = await Book.findOne(req.params.isbn);
         return res.json({ book });
     } catch (err) {
         return next(err);
